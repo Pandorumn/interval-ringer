17838ec57923ad92bd5732aa5b91ba41f29a9767
Pandorumn <70848056+Pandorumn@users.noreply.github.com>
2022-06-02 17:52:48 +0300

    ring the bell

--- script.js
+++ script.js
@@ -2,10 +2,40 @@ const containerElem = document.querySelector(".time")
 const hoursElem = document.querySelector(".hours")
 const minutesElem = document.querySelector(".minutes")
 const dotsElem = document.querySelector(".dots")
+const audioElem = document.querySelector("audio")
+const iconMuted = document.querySelector(".icon-muted")
+const iconSpeaker = document.querySelector(".icon-speaker")
+
+const highlightedClassName = "highlighted"
+const hiddenClassName = "hidden"
 
 dotsElem.textContent = ":"
 
-const highlightedClassName = "highlighted"
+function ring() {
+  const audio = new Audio("pristine-609.mp3")
+  audio.play()
+}
+function unmute() {
+  iconMuted.classList.add(hiddenClassName)
+  iconSpeaker.classList.remove(hiddenClassName)
+}
+document.addEventListener("click", unmute)
+iconSpeaker.addEventListener("click", ring)
+
+function useMunitesState() {
+  let previousM = 0
+
+  function setPreviousM(value) {
+    previousM = value
+  }
+  function getPreviousM() {
+    return previousM
+  }
+
+  return { setPreviousM, getPreviousM }
+}
+
+const { getPreviousM, setPreviousM } = useMunitesState()
 
 setInterval(() => {
   const h = new Date().getHours()
@@ -15,9 +45,13 @@ setInterval(() => {
   minutesElem.textContent = m < 10 ? "0" + m : m
 
   const isTimeToRing = !(m % 20)
+  const isRinged = !(getPreviousM() % 20)
+
+  setPreviousM(m)
 
   if (isTimeToRing) {
     containerElem.classList.add(highlightedClassName)
+    !isRinged && ring()
   } else {
     containerElem.classList.remove(highlightedClassName)
   }
